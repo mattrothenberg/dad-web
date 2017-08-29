ab65fbef6fd0f38be44e7a5da2e07d10e5ca3b39
Matt Rothenberg <mattrothenberg@gmail.com>
2017-08-28 21:26:29 -0400

    Lazy load images

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,12 +1,15 @@
 //= require vendor/jquery
 //= require vendor/flickity
 //= require vendor/flickity-imagesloaded
+//= require vendor/lazyload.min
 
 $(document).ready(function() {
 	var galleries = $('.gallery');
 	
 	if( galleries.length ) {
 		initGalleries();
+	} else {
+		var myLazyLoad = new LazyLoad();
 	}
 
 	function initGalleries() {
